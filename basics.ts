11c1baa0b8831131261f9288cdd61ad9dd05fa24
naekit <mykyta.onishchuck@code.berlin>
2023-01-29 19:11:43 -0500

    generics and function special return type

--- basics.ts
+++ basics.ts
@@ -24,3 +24,39 @@ any = false
 function warnUser(): void {
 	console.log("This is my warning message")
 }
+// More complex types
+let hobbies: string[] = ["Sports", "Cooking"]
+
+let person: { name: string; age: number } = {
+	name: "Niko",
+	age: 26,
+}
+
+let people: { name: string; age: number }[] = []
+
+// Type inference or union types
+let course: string | number = "TS - Guide"
+course = 888
+// Type alias
+type Person = { name: string; age: number }
+let anotherPerson: Person = {
+	name: "Niko",
+	age: 26,
+}
+let morePeople: Person[] = []
+// Function types
+function add(a: number, b: number): number {
+	return a + b
+}
+
+function printThis(value: any): void {
+	console.log(value)
+}
+
+// Generics
+function insertAtBeginning<T>(array: T[], value: T) {
+	const newArray = [value, ...array]
+	return newArray
+}
+const demoArr = [1, 2, 3]
+const updatedArr = insertAtBeginning(demoArr, -1) // [-1, 1, 2, 3]
